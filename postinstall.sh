cbae40a5e03c861218a979a33bab5b78b77d0c51
Paul Puey <paul@edgesecure.co>
2018-05-24 16:37:46 -0700

    Add functionality to do basic send and recv using bitcoin plugin

--- postinstall.sh
+++ postinstall.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 rn-nodeify --hack
+rm -rf ./node_modules/bcoin/.babelrc ./node_modules/edge-currency-bitcoin/node_modules/bcoin/.babelrc
+rm -rf ./node_modules/bccoin/.babelrc ./node_modules/edge-currency-bitcoin/node_modules/bccoin/.babelrc
+rm -rf ./node_modules/lcoin/.babelrc ./node_modules/edge-currency-bitcoin/node_modules/lcoin/.babelrc
+
+# Remove inclusion of c++_shared.so library since we are using jsc-android which already includes it
+sed "s/\,[[:space:]]'-DANDROID_STL=c++_shared'//g" ./node_modules/react-native-fast-crypto/android/build.gradle > build.gradle
+mv build.gradle ./node_modules/react-native-fast-crypto/android/build.gradle
--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@
  * 
  */
 
+import '../shim.js'
 import React, { Component } from 'react'
+import { Button } from 'react-native'
+import { makeEdgeContext, type EdgeSpendInfo } from 'edge-core-js'
 import { LoginScreen } from 'edge-login-ui-rn'
-import { makeEdgeContext } from 'edge-core-js'
+import {
+  bitcoincashCurrencyPluginFactory,
+} from 'edge-currency-bitcoin'
+
 import {
   Platform,
   StyleSheet,
@@ -14,43 +20,132 @@ import {
   View
 } from 'react-native'
 
-function setupCore () {
-  return makeEdgeContext({
-    // Replace this with your own API key from https://developer.airbitz.co:
-    apiKey: '0b5776a91bf409ac10a3fe5f3944bf50417209a0',
-    appId: 'com.mydomain.myapp',
-    vendorName: 'Chain Net',
-    vendorImageUrl: 'https://airbitz.co/go/wp-content/uploads/2016/10/GenericEdgeLoginIcon.png'
-  })
-}
-
 export default class App extends Component{
   constructor (props) {
     super(props)
     this.state = {
       context: null,
       account: null,
+      walletId: null,
+      wallet: null,
+      content: ''
     }
     // Creating the context is async, so we store it in our state:
-    setupCore().then(context => this.setState(state => ({ ...state, context })))
-  }
-  onLogin = (error = null, accountObject) => {
-    this.setState({
-      account: accountObject
+    makeEdgeContext({
+      // Replace this with your own API key from https://developer.airbitz.co:
+      apiKey: '0b5776a91bf409ac10a3fe5f3944bf50417209a0',
+      appId: 'com.mydomain.myapp',
+      vendorName: 'Chain Net',
+      vendorImageUrl: 'https://airbitz.co/go/wp-content/uploads/2016/10/GenericEdgeLoginIcon.png',
+      plugins: [bitcoincashCurrencyPluginFactory]
+    }).then(context => {
+      this.setState({ context })
     })
   }
-  renderLoginApp = () => {
-    if (this.state.account){
-      return <Text style={styles.welcome}>Logged In</Text>
+
+  onLogin = (error = null, account) => {
+    if (error) {
+      console.log(error)
     }
-    if (this.state.context && !this.state.account) {
+    if (!this.state.account) {
+      this.setState({account})
+    }
+    if (!this.state.walletId) {
+      // Check if there is a wallet, if not create it
+      let walletInfo = account.getFirstWalletInfo('wallet:bitcoincash')
+      if (walletInfo) {
+        this.setState({walletId: walletInfo.id})
+      } else {
+        account.createCurrencyWallet('wallet:bitcoincash', {
+          name: 'My First Wallet',
+          fiatCurrencyCode: 'iso:USD'
+        }).then(wallet => {
+          this.setState({ wallet })
+          this.setState({walletId: wallet.id})
+        })
+      }
+    }
+  }
+
+  accountOptions = {
+    callbacks: {
+      onKeyListChanged: () => {
+        this.logger('onKeyListChanged:')
+        if (this.state.account && !this.state.wallet && this.state.walletId) {
+          // Try to grab the wallet from currencyWallets
+          const wallet = this.state.account.currencyWallets[this.state.walletId]
+          if (wallet) {
+            this.setState({ wallet })
+          }
+        }
+      },
+      onAddressesChecked: (walletId, progressRatio) => {
+        if (walletId === this.state.walletId) {
+          this.logger(`onAddressesChecked: ${walletId.slice(0, 6)} ${progressRatio}`)
+          if (progressRatio === 1) {
+            this.state.wallet.getReceiveAddress().then(address => {
+              this.logger(`address ${address.publicAddress}`)
+            })
+          }
+        }
+      },
+      onBalanceChanged: (walletId, currencyCode, nativeBalance) => {
+        if (walletId === this.state.walletId) {
+          this.logger(`onBalanceChanged: ${walletId.slice(0, 6)} ${currencyCode} ${nativeBalance}`)
+        }
+      },
+      onNewTransactions: (walletId, edgeTransactions) => {
+        if (walletId === this.state.walletId) {
+          this.logger(`onNewTransactions: ${walletId.slice(0, 6)} ${edgeTransactions[0].nativeAmount}`)
+        }
+      }
+    }
+  }
+
+  onPressButton = async () => {
+    const spendInfo: EdgeSpendInfo = {
+      currencyCode: 'BCH',
+      spendTargets: [{
+        publicAddress: 'bitcoincash:qrpx5e79hmrzmyfrfpyxunud9e6qsdzj0vuss2mq58',
+        nativeAmount: '10000' // 1 mBCH ($.10 USD)
+      }]
+    }
+
+    try {
+      let tx = await this.state.wallet.makeSpend(spendInfo)
+      tx = await this.state.wallet.signTx(tx)
+      tx = await this.state.wallet.broadcastTx(tx)
+      await this.state.wallet.saveTx(tx)
+      this.logger('Tx sent')
+    } catch (e) {
+      this.logger(e)
+    }
+  }
+
+  renderLoginApp = () => {
+    // No context.. We're still initializing Edge Core
+    if (!this.state.context) {
+      return <Text style={styles.welcome}>Loading...</Text>
+    }
+
+    // No account, we need to show login screen
+    if (!this.state.account) {
       return <LoginScreen
       context={this.state.context}
-      onLogin={this.onLogin.bind(this)}
-      accountOptions={{}}
+      onLogin={this.onLogin}
+      // accountOptions={{}}
+      accountOptions={this.accountOptions}
       />
     }
-    return <Text style={styles.welcome}>Loading</Text>
+
+    // We're logged in!!
+    return (
+      <View>
+        <Button onPress={this.onPressButton} title={'Spend'}/>
+        <Text style={styles.welcome}>Logged in</Text>
+        <Text style={styles.content}>{this.state.content}</Text>
+      </View>
+    )
   }
   render() {
     return (
@@ -59,18 +154,31 @@ export default class App extends Component{
       </View>
     );
   }
+  logger (t) {
+    if (typeof t === 'object') {
+      t = JSON.stringify(t)
+    }
+    this.setState({ content: this.state.content + t + '\n'})
+    console.log(t)
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    marginTop: 20,
+    // justifyContent: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10
+  },
+  content: {
+    fontSize: 15,
+    textAlign: 'left',
+    margin: 5
   }
 });
