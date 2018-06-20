5f4f92863ccd961a103da1a562ba8e6109c16dcb
William Swanson <swansontec@gmail.com>
2018-06-20 11:29:41 -0700

    Run `react-native link`

--- android/app/src/main/java/com/demo/MainApplication.java
+++ android/app/src/main/java/com/demo/MainApplication.java
@@ -3,6 +3,25 @@ package com.demo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.tradle.react.UdpSocketsModule;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
+import cl.json.RNSharePackage;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
+import com.beefe.picker.PickerViewPackage;
+import com.github.xinthink.rnmk.ReactMaterialKitPackage;
+import io.fixd.rctlocale.RCTLocalePackage;
+import com.BV.LinearGradient.LinearGradientPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.slowpath.hockeyapp.RNHockeyAppPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lynxit.contactswrapper.ContactsWrapperPackage;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.cmcewen.blurview.BlurViewPackage;
+import com.jamesisaac.rnbackgroundtask.BackgroundTaskPackage;
+import com.transistorsoft.rnbackgroundfetch.RNBackgroundFetchPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
 import co.airbitz.AbcCoreJsUi.AbcCoreJsUiPackage;
 import com.peel.react.TcpSocketsModule;
 import com.bitgo.randombytes.RandomBytesPackage;
@@ -28,6 +47,25 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new UdpSocketsModule(),
+            new RNSoundPackage(),
+            new RCTSplashScreenPackage(),
+            new RNSharePackage(),
+            new ReactNativePushNotificationPackage(),
+            new PickerViewPackage(),
+            new ReactMaterialKitPackage(),
+            new RCTLocalePackage(),
+            new LinearGradientPackage(),
+            new ImagePickerPackage(),
+            new RNHockeyAppPackage(),
+            new RNDeviceInfo(),
+            new ContactsWrapperPackage(),
+            new ReactNativeContacts(),
+            new RCTCameraPackage(),
+            new BlurViewPackage(),
+            new BackgroundTaskPackage(),
+            new RNBackgroundFetchPackage(),
+            new VectorIconsPackage(),
             new AbcCoreJsUiPackage(),
             new TcpSocketsModule(),
             new RandomBytesPackage(),
